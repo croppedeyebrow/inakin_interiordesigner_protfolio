8c40a5e65e7b8054cb196b4963d0483f12ea3482
Croppedeyebrow <lee940706@gmail.com>
2024-06-09 15:52:20 +0900

    0609

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -4,7 +4,7 @@ type SectionHeadingProps = { children: React.ReactNode };
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
   return (
-    <h2 className=" text-3xl font-medium capitalize mb-8 text-black">
+    <h2 className=" text-3xl font-bold capitalize mb-8 text-black text-center">
       {children}
     </h2>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import interiorporject01Img from "@/public/Interiorporject01.jpg";
+import interiorporject02Img from "@/public/Interiorporject02.jpg";
+import interiorporject03Img from "@/public/Interiorporject03.jpg";
 
 export const links = [
   {
@@ -62,25 +62,25 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Interior Design Project 01",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    tags: ["AutoCAD", "SketchUp", "3dMAX", "Vray", "Photoshop", "Illustrator"],
+    imageUrl: interiorporject01Img,
   },
   {
-    title: "rmtDev",
+    title: "Interior Design Project 02",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    tags: ["AutoCAD", "SketchUp", "3dMAX", "Vray", "Photoshop", "Illustrator"],
+    imageUrl: interiorporject02Img,
   },
   {
-    title: "Word Analytics",
+    title: "Interior Design Project 03",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    tags: ["AutoCAD", "SketchUp", "3dMAX", "Vray", "Photoshop", "Illustrator"],
+    imageUrl: interiorporject03Img,
   },
 ] as const;
 
